94f467cb47431575d3254ec533d8268c7d9e70ae
Sray <>
2016-11-06 17:30:37 +0100

    test socket io c#

---
+++ Client/BattleNetworkClient/BattleNetworkClient/Assets/_Project/Scripts/TestSocketIO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Quobject.SocketIoClientDotNet.Client;
+
+public class TestSocketIO : MonoBehaviour {
+    Socket socket;
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+
+    void test()
+    {
+        socket = IO.Socket("http://localhost");
+        socket.On(Socket.EVENT_CONNECT, () =>
+        {
+            socket.Emit("hi");
+
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
