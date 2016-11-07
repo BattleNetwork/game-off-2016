f9db7689b39e6fb18538159d677da137395d53b9
MrGrimmy <>
2016-11-08 00:19:34 +0100

    - Update sequence loadingScreen. - Nouveau bruit de PC. - suppression des ancien asset audio.

--- Client/BattleNetworkClient/BattleNetworkClient/Assets/_Project/Scripts/Bootup.cs
+++ Client/BattleNetworkClient/BattleNetworkClient/Assets/_Project/Scripts/Bootup.cs
@@ -4,21 +4,28 @@ using UnityEngine.UI;
 
 public class Bootup : MonoBehaviour {
 
-    public TextAsset sequence;
+    public TextAsset bootSequenceText;
     public string[] textLines;
     public Text text;
+    public Text logoText;
     public float waitTime;
+    public float speed;
+
+    public Image logo;
 
     private int currentLine = -1;
     private int endLine;
+    private int sequence;
     private float actTime;
+
 
 
 	// Use this for initialization
 	void Start ()
     {
-        textLines = (sequence.text.Split('\n'));
+        textLines = (bootSequenceText.text.Split('\n'));
         endLine = textLines.Length;
+        logoText.enabled = false;
 
 	}
 	
@@ -26,17 +33,28 @@ public class Bootup : MonoBehaviour {
 	void Update ()
     {
         actTime += Time.deltaTime;
-        Debug.Log(actTime);
-        foreach (string i in textLines)
+        if (actTime >= waitTime & logo.rectTransform.localScale.y < 1f)
         {
-            if (actTime >= waitTime)
+            logo.rectTransform.localScale += new Vector3(0,0.1f,0);
+            Debug.Log(logo.rectTransform.localScale);
+        }
+        else if(logo.rectTransform.localScale.y >= 1.0f)
+        {
+            sequence += 1;
+        }
+        if (sequence == 1)
+        {
+            logoText.enabled = true;
+            sequence += 1;
+            actTime = 0;
+        }
+        if (actTime >= waitTime & sequence > 2)
+        {
+            if (currentLine < endLine)
             {
-                if (currentLine <= endLine)
-                {
-                    currentLine += 1;
-                    text.text = text.text + "\n" + textLines[currentLine];
-                    actTime = 0;
-                }
+                currentLine += 1;
+                text.text = text.text + "\n" + textLines[currentLine];
+                actTime = 0;
             }
         }
     }
